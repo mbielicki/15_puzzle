cc86a67c17963ef3d700fd3bf9da78095f101320
Michał Bielicki <mbielicki2912@gmail.com>
2025-11-08 17:29:15 +0100

    Refactor BFS movement logic to prevent reversing moves

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -10,19 +10,19 @@ def bfs(puzzle: Puzzle) -> Puzzle:
         current_puzzle = queue.pop(0)
         if current_puzzle.is_solved():
             return current_puzzle
-        if current_puzzle.can_move("U"):
+        if current_puzzle.can_move("U") and current_puzzle.previous_move() != "D":
             new_puzzle = current_puzzle.copy()
             new_puzzle.up()
             queue.append(new_puzzle)
-        if current_puzzle.can_move("D"):
+        if current_puzzle.can_move("D") and current_puzzle.previous_move() != "U":
             new_puzzle = current_puzzle.copy()
             new_puzzle.down()
             queue.append(new_puzzle)
-        if current_puzzle.can_move("L"):
+        if current_puzzle.can_move("L") and current_puzzle.previous_move() != "R":
             new_puzzle = current_puzzle.copy()
             new_puzzle.left()
             queue.append(new_puzzle)
-        if current_puzzle.can_move("R"):
+        if current_puzzle.can_move("R") and current_puzzle.previous_move() != "L":
             new_puzzle = current_puzzle.copy()
             new_puzzle.right()
             queue.append(new_puzzle)
